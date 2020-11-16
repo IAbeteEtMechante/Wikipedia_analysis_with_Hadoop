65898dc5b0c6ee784fd1664fe527ffed1c814bb5
Duc Pham <pham.huuduc611@gmail.com>
2020-11-16 23:25:48 +0700

    Add files from wiki repo

---
+++ src/main/java/App.java
@@ -0,0 +1,21 @@
+/**
+ * Get Data from Wikipedia and Analyse Trends in the data
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+    	/*
+    	 * Uncomment each of these to analyze the data
+    	 */
+    	
+        createBigData.main(null);
+        TotalPageViews.main(null);
+        //TopNPageViews.main(null);
+        //LastNPageViews.main(null);
+        //AveragePageViews.main(null);
+        //TopNAveragePageViews.main(null);
+        
+    }
+}
