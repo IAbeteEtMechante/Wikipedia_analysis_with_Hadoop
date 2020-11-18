71885277fc6982aab14f0a16e7a6201a44d2e51a
patriciaporal <patriciaann.poral@gmail.com>
2020-11-19 01:28:11 +0800

    Project

--- src/main/java/space/harbour/java/project1/AveragePageViews.java
+++ src/main/java/space/harbour/java/project1/AveragePageViews.java
@@ -1,5 +1,6 @@
 package space.harbour.java.project1;
 
+import java.io.IOException;
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -11,13 +12,13 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import java.io.IOException;
 
 public class AveragePageViews {
     public static class AveragePageViewsMap extends Mapper<LongWritable, Text, Text, LongWritable> {
 
         @Override
-        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        public void map(LongWritable key, Text value, Context context)
+                throws IOException, InterruptedException {
             String delims = ",";
             String[] wikiData = StringUtils.split(value.toString(), delims);
             Text article = new Text();
@@ -30,7 +31,8 @@ public class AveragePageViews {
         }
     }
 
-    public static class AveragePageViewsReduce extends Reducer<Text, LongWritable, Text, DoubleWritable> {
+    public static class AveragePageViewsReduce extends
+            Reducer<Text, LongWritable, Text, DoubleWritable> {
         public void reduce(Text key, Iterable<LongWritable> values, Context context)
                 throws IOException, InterruptedException {
             Long views = (long) 0;
@@ -39,12 +41,13 @@ public class AveragePageViews {
                 views += t.get();
                 count++;
             }
-            Double average = (double) views/ (double)count;
+            Double average = (double)views/(double)count;
             context.write(key, new DoubleWritable(average));
         }
     }
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException,
+            InterruptedException {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "countWikipedia");
         job.setJarByClass(AveragePageViews.class);
