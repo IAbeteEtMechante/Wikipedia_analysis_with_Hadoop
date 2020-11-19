f420fd780346b41f51c45b81d76aca3273aa355f
IAbeteEtMechante <pierre.machine.learning@gmail.com>
2020-11-19 14:51:33 +0700

    Add SumPageViews and its tests

---
+++ src/test/java/AveragePageViewsTest.java
@@ -0,0 +1,14 @@
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class AveragePageViewsTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void main() {
+    }
+}
---
+++ src/test/java/SumPageViewsTest.java
@@ -0,0 +1,14 @@
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class SumPageViewsTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void main() {
+    }
+}
