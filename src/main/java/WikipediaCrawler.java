dbe2af5698818344ef01fda0056e13eb310f7290
Duc Pham <pham.huuduc611@gmail.com>
2020-11-21 14:06:52 +0700

    Edit input for WikipediaCrawler and its test

--- src/main/java/WikipediaCrawler.java
+++ src/main/java/WikipediaCrawler.java
@@ -1,41 +1,31 @@
-/*
- * Get each day's page views for topics mentioned in main method
- */
 
-import java.io.*;
-import java.lang.reflect.Array;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
-* @author: Patricia, Duc, Pierre
-*/
-
 public class WikipediaCrawler {
-	/**
-	* This is the program for getting number of views
-	*from different articles from Wikipedia
-	*/
 
     private final String userAgent = "Mozilla/5.0";
 
     public static void main(String[] args) throws Exception {
-        prepUrl("articles");
+        prepUrl("articles", "2015100100", "2015103100");
     }
 
-    /**
-     * Send GET request to get the data
-     */
     private String sendGet(String url) throws Exception {
 
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-	
+
         // optional default is GET
         con.setRequestMethod("GET");
 
@@ -83,42 +73,36 @@ public class WikipediaCrawler {
         return result;
     }
 
-    /**
-     * Prepare the Wikipedia API URL
-     */
-    public static String prepUrl(String filename) throws Exception {
+    public static String prepUrl(String filename,
+                                 String startDates,
+                                 String endDates) throws Exception {
         BufferedReader inputFileReader = new BufferedReader(new FileReader(filename));
         List<String> lines = new ArrayList<>();
         String line = inputFileReader.readLine();
 
-        while(line != null) {
+        while (line != null) {
             lines.add(line);
             line = inputFileReader.readLine();
         }
         inputFileReader.close();
 
         String[] articles = lines.toArray(new String[]{});
-        String[] startDates = {"2015100100"};
-        String[] endDates = {"2015103000"};
         WikipediaCrawler http = new WikipediaCrawler();
         String response = "";
         for (int i = 0; i < articles.length; i++) {
-            for (int j = 0; j < startDates.length; j++) {
-                String url1 =
-                        "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/";
-                String url2 = articles[i]; //Article name
-                String url3 = "/daily/";
-                String url4 = startDates[j]; //Start Date
-                String url5 = "/";
-                String url6 = endDates[j]; //End Date
-                StringBuilder sb = new StringBuilder();
-                sb.append(url1).append(url2).append(url3).append(url4).append(url5).append(url6);
-                String res = sb.toString();
-                response = http.sendGet(res);
-                // System.out.println(response);
-                parseFromJsonResponse(response);
-
-            }
+            String url1 =
+                    "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/";
+            String url2 = articles[i]; //Article name
+            String url3 = "/daily/";
+            String url4 = startDates; //Start Date
+            String url5 = "/";
+            String url6 = endDates; //End Date
+            StringBuilder sb = new StringBuilder();
+            sb.append(url1).append(url2).append(url3).append(url4).append(url5).append(url6);
+            String res = sb.toString();
+            response = http.sendGet(res);
+            // System.out.println(response);
+            parseFromJsonResponse(response);
         }
         return response;
     }
