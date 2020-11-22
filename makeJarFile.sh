6442376c0ff89df2150ddaf02609870273a252ef
IAbeteEtMechante <pierre.machine.learning@gmail.com>
2020-11-22 13:45:20 +0700

    make modules

--- makeJarFile.sh
+++ makeJarFile.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 javac -classpath $(find /usr/local/hadoop -name "*.jar" | awk '{printf("%s:",$1)} END{printf("\n")}' | sed 's/:$//') *.java
 jar cvf Job.jar *.class
+rm -rf *.class
