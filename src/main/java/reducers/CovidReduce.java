7596a6ca52eba434ed1de95106ba52f159430fa8
Duc Pham <pham.huuduc611@gmail.com>
2020-11-24 11:20:13 +0700

    Add mapreduce codes for Covid articles view

---
+++ src/main/java/reducers/CovidReduce.java
@@ -0,0 +1,20 @@
+package reducers;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class CovidReduce extends Reducer<Text, LongWritable, Text, LongWritable> {
+
+    public void reduce (Text key, Iterable<LongWritable> values, Context context)
+            throws IOException, InterruptedException {
+
+        long views = (long) 0;
+        for (LongWritable t : values) {
+            views += t.get();
+        }
+        context.write(key, new LongWritable(views));
+    }
+}
